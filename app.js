cffa5b88034003cdf0526c73b3f42d0c82ee6d56
DorKatzir <DorKatzir@users.noreply.github.com>
2020-04-10 17:08:05 +0300

    js cardArray defined

---
+++ app.js
@@ -0,0 +1,51 @@
+//card options
+const cardArray = [
+    {
+        name: 'avocado',
+        img: 'images/avocado.png' // relative path
+    },
+    {
+        name: 'avocado',
+        img: 'images/avocado.png' // relative path
+    },
+    {
+        name: 'grapes',
+        img: 'images/grapes.png'
+    },
+    {
+        name: 'grapes',
+        img: 'images/grapes.png'
+    },
+    {
+        name: 'melon',
+        img: 'images/melon.png'
+    },
+    {
+        name: 'melon',
+        img: 'images/melon.png'
+    },
+    {
+        name: 'papaya',
+        img: 'images/papaya.png'
+    },
+    {
+        name: 'papaya',
+        img: 'images/papaya.png'
+    },
+    {
+        name: 'strewberry',
+        img: 'images/strewberry.png'
+    },
+    {
+        name: 'strewberry',
+        img: 'images/strewberry.png'
+    },
+    {
+        name: 'watermelon',
+        img: 'images/watermelon.png'
+    },
+    {
+        name: 'watermelon',
+        img: 'images/watermelon.png'
+    }
+]
