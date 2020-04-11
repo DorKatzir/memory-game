07439e6b92bf7c4d20eca03613a6bfebff7a55f5
DorKatzir <DorKatzir@users.noreply.github.com>
2020-04-12 01:47:34 +0300

    CSS more styles and grid

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ function checkForMatch(){
     cardsChosen = [];
     cardsChosenId = [];
 
+    
     resultDisplay.textContent = cardsWon.length;
     if(cardsWon.length === cardArray.length/2){
         resultDisplay.textContent = 'Congratulations! You found them all!';
